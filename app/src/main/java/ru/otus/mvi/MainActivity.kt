7c53e67119f3d87757d2027a0a529d11291a359f
Maxim Kachinkin <m.kachinkin@dodobrands.io>
2025-09-28 15:03:16 +0300

    Fix edge to edge

--- app/src/main/java/ru/otus/mvi/MainActivity.kt
+++ app/src/main/java/ru/otus/mvi/MainActivity.kt
@@ -1,14 +1,17 @@
 package ru.otus.mvi
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
     }
 }
